f0913f7bc441a6ac01b2dc4a8654c040b6a26678
Barbarubia <dnl.desimone@gmail.com>
2022-03-01 15:48:09 -0300

    Palidroma: a century later finally it works!

--- js/script.js
+++ js/script.js
@@ -4,11 +4,6 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
-// Seleziono l'elemento HTML in cui l'utente scrive la parola
-let inputBox = document.getElementById('word');
-
-// Definisco una variabile per la parola inserita dall'utente
-let userWord = inputBox.value;
 
 // Seleziono l'elemento HTML bottone al cui click effettuerò i controlli sulla parola inserita
 let buttonVerifyWord = document.getElementById('button-verify-word');
@@ -16,29 +11,50 @@ let buttonVerifyWord = document.getElementById('button-verify-word');
 // Seleziono l'elemento HTML dove stamperò il risultato
 let outputArea = document.getElementById('word-output');
 
-// Genero la funzione che verifica se la parola è palindroma
-function isPalindrome(verifyWord) {
-    // Scompongo la parola inserita in un array di caratteri tutti minuscoli
-    let arrayWord = [...verifyWord.toLowerCase()];
+// Eseguo la funzione che al click del bottone verifica se la parola inserita è palindroma
+buttonVerifyWord.addEventListener('click', isPalindrome);
 
-    // Definisco un array, inizialmente vuoto, per generare un array reverse del precedente
-    let arrayReverseWord = [];
 
-    // Creo un ciclo for che prende i singoli elementi dell'array creato a partire dall'ultimo e li mette in un nuovo array
-    for (let positionCharacter = arrayWord.length - 1; positionCharacter = 0; positionCharacter--) {
-        arrayReverseWord.push(arrayWord[positionCharacter]);
-    }
 
-    // Confronto l'uguaglianza tra i 2 array per verificare se la parola è palindroma e stampo il risultato
-    if (arrayWord == arrayReverseWord) {
-        outputArea.innerHTML = `La parola "${verifyWord}" è palindroma.`;
+
+
+// Definizione della funzione
+function isPalindrome() {
+    // Seleziono l'elemento HTML in cui l'utente scrive la parola
+    let inputBox = document.getElementById('word');
+
+    // Definisco una variabile per la parola inserita dall'utente
+    let userWord = inputBox.value;
+
+    // Scompongo la parola inserita in un array di caratteri
+    let arrayWord = [...userWord];
+
+    // Inverto l'ordine degli elementi dell'array dei singoli caratteri della parola
+    let arrayReversedWord = arrayWord.reverse();
+
+    // Ricompongo gli elementi dell'array invertito in un'unica stringa
+    let reversedWord = arrayReversedWord.join('');
+
+    // Confronto l'uguaglianza tra le due parole trasformate in minuscolo per confrontarne l'uguaglianza e stampo il risultato, prima però faccio una verifica sulla lunghezza della parola perché la verifica ha senso solo per parole di almeno due caratteri
+    if (userWord.length == 0) {
+        outputArea.innerHTML = `Non hai digitato nulla.`;
+    } else if (userWord.length < 2) {
+        outputArea.innerHTML = `Inserisci una parola di almeno 2 caratteri.`;
+    } else if (userWord.toLowerCase() == reversedWord.toLowerCase()) {
+        outputArea.innerHTML = `La parola "${userWord}" è palindroma.`;
     } else {
-        outputArea.innerHTML = `La parola "${verifyWord}" non è palindroma.`;
+        outputArea.innerHTML = `La parola "${userWord}" non è palindroma.`;
     }
+
+    // Ripulisco l'input
+    inputBox.value = ``;
+
 }
 
-// Eseguo la funzione creata al click del bottone
-buttonVerifyWord.addEventListener('click', isPalindrome(userWord));
+
+
+
+
 
 /*
 // Voglio che la parola inserita sia composta esclusivamente da caratteri alfabetici (maiuscolo o minuscolo non importa)
